805a6ef4671ae87b3a252963cef194f93619b542
Peter Banka <peter.banka@gmail.com>
2011-12-30 05:57:36 +0000

    Fiddling with the build

--- server/dodo.py
+++ server/dodo.py
@@ -1,11 +1,50 @@
 import os
+from commands import getstatusoutput as gso
+import sys
 
+OK = 0
 SYSTEM_ROOT = os.environ.get("VIRTUAL_ENV", '/')
 HTTPD_PATH = os.path.join(SYSTEM_ROOT, 'etc', 'httpd')
 COMPONENTS = ['server']
 VAR_RUN = os.path.join(SYSTEM_ROOT, "var", "run")
 HTTPD_PATH = os.path.join(SYSTEM_ROOT, 'etc', 'httpd')
 DEWIT = "python $(which doit)"
+PYTHON_EXE = sys.executable
+VERSION_TEMPLATE = """\
+VERSION_INFO = {
+    "Revision": "%s",
+}
+"""
+DJANGO_PREAMBLE = 'DJANGO_SETTINGS_MODULE="dri_server.web.settings" '
+NOSE_CMD = '%s %s `which nosetests` --exe --with-id ' % (DJANGO_PREAMBLE, PYTHON_EXE)
+QUICKTEST_ADDENDUM = " -vv -s --stop"
+
+##################### EXCEPTIONS
+
+class BuildConfigurationException(Exception):
+    "Something is wrong with the build, so throw an exeption"
+    def __init__(self, msg):
+        Exception.__init__(self)
+        self.msg = msg
+    def __str__(self):
+        return "BuildConfigurationException: %s" % self.msg
+    def __repr__(self):
+        return self.__str__()
+
+##################### SUPPORT FUNCTIONS
+
+def get_version():
+    "Get the SVN revision number for this build"
+    status, output = gso("git rev-parse --short HEAD")
+    if status != OK:
+        raise BuildConfigurationException("Unable to obtain SVN version")
+    version_info = output.strip()
+    print ">>> (%s)" % version_info
+    version_python_code = VERSION_TEMPLATE % version_info
+    open("src/_version.py", 'w').write(version_python_code)
+
+def rmrf(filename):
+    return "rm -rf %s" % filename
 
 def _make_link(source, dest):
     if not os.path.islink(dest):
@@ -19,8 +58,8 @@ def configure_apache():
     _make_link("%s/var/log/httpd" % SYSTEM_ROOT,  "%s/logs" % HTTPD_PATH)
     _make_link("%s/var/run/httpd" % SYSTEM_ROOT,  "%s/run" % HTTPD_PATH)
     _make_link("/usr/lib64/httpd/modules", "%s/modules" % (HTTPD_PATH))
-    os.system('cp ../defaults/httpd.conf %s/conf' % HTTPD_PATH)
-    os.system('cp ../defaults/tnt.conf %s/conf.d/tnt.conf' % HTTPD_PATH)
+    #os.system('cp ../defaults/httpd.conf %s/conf' % HTTPD_PATH)
+    os.system('cp ../dri.conf %s/conf.d/dri.conf' % HTTPD_PATH)
     os.environ["DRI_ROOT"] = SYSTEM_ROOT
     os.system('export DRI_ROOT="%s"' % SYSTEM_ROOT)
 
@@ -31,6 +70,43 @@ def server_command(command, quiet = False):
     os.system(cmd)
     os.system("httpd -d %s -k %s" % (HTTPD_PATH, command))
 
+def task_version():
+    "prepare to build"
+    return {"actions": [
+                        #(get_file, ["_project_info.py"]),
+                        #(get_file, ["setup.py"]),
+                        #"cp _project_info.py src",
+                        (get_version,),
+                       ],
+           }
+
+def task_clean_build():
+    "Clean out old builds"
+    def rmrf(filename):
+        return "rm -rf %s" % filename
+
+    return {"actions": map(rmrf, [
+                        "build",
+                        "dist",
+                        "MANIFEST",
+                        "MANIFEST.in",
+                        "_manifest_test",
+                        #"_project_info.py",
+                        #"setup.py",
+                        #"src/_project_info.py",
+                        "src/_version.py",
+                        #"testdb",
+                       ]),
+           }
+
+def task_build():
+    "Builds the code"
+    return {"actions": [
+                        "%s setup.py build" % PYTHON_EXE,
+                       ],
+            "task_dep": ["clean_build", "version"],
+           }
+
 def assure_virtual():
     "make sure we're in a virtual environment"
     if os.environ.get("VIRTUAL_ENV"):
@@ -38,7 +114,7 @@ def assure_virtual():
     return False
 
 def task_set_config_data():
-    "Sets the data in /etc/tnt/server.yml"
+    "Sets up configuration data"
     return {
             #"targets": ["/var/deploy/rest_api"],
             "actions": [(configure_apache,)],
@@ -77,4 +153,33 @@ def task_cleanup():
                         'rm -rf %s/*.pid' % VAR_RUN,
                        ],
            }
+
+def task_install():
+    "Install the code."
+    return {"actions": [
+                        "%s setup.py install" % PYTHON_EXE,
+                       ],
+            "task_dep": ["build"],
+           }
+
+def task_quicktest():
+    "Run quick unit tests without any of the other nonsense."
+    def quicktest(test_id):
+        cmd = "%s %s %s" % (NOSE_CMD, test_id, QUICKTEST_ADDENDUM)
+        print '-------------------------------------------------------'
+        print "Here's the command: %s" % cmd
+        print '-------------------------------------------------------'
+        os.system(cmd)
+
+    return {
+            "actions": [(quicktest,)],
+            "params": [{
+                        'name': 'test_id',
+                        'short': 'i',
+                        'default': '',
+                       }],
+            "verbosity": 2,
+            "task_dep": ["install", "set_config_data"],
+           }
+
 
--- server/src/_version.py
+++ server/src/_version.py
@@ -1,3 +1,3 @@
 VERSION_INFO = {
-    "Revision": "82d21b4",
+    "Revision": "bc113be",
 }
