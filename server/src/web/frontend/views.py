6fea33b6036f66af5ed525ac243d406fb9f08c74
Peter Banka <peter.banka@gmail.com>
2011-12-30 20:18:09 -0800

    feel like committing

---
+++ server/src/web/frontend/views.py
@@ -0,0 +1,6 @@
+# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import loader, Context, RequestContext
+
+def first_page(request):
+    return render_to_response("frontend/my_first_page.html", context_instance=RequestContext(request))
