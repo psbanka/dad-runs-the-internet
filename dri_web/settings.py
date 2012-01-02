4e20f08b4ee9fd027966816e6b30b500efd9bd4d
Peter Banka <peter.banka@gmail.com>
2012-01-02 14:50:48 -0800

    static files and heroku.

--- dri_web/settings.py
+++ dri_web/settings.py
@@ -52,8 +52,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '%s/dri/media/' % SYSTEM_ROOT
-print ">>>>>>>>>>>",MEDIA_ROOT
+MEDIA_ROOT = '~/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -114,7 +113,7 @@ DOJANGO_BASE_MEDIA_ROOT="/lib"
 DOJANGO_BASE_DOJO_ROOT="/lib/dojo"
 
 TEMPLATE_DIRS = (
-    "%s/dri/media" % SYSTEM_ROOT,
+    "~/media",
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
