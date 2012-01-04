fefc77398699ac6ea9326a593c69629161b82f4e
Peter Banka <peter.banka@gmail.com>
2012-01-03 23:14:44 -0800

    Unit tests are working!

--- dri_web/settings.py
+++ dri_web/settings.py
@@ -53,7 +53,6 @@ USE_L10N = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = '/app/media/'
-print ">>> MEDIA_ROOT:",MEDIA_ROOT
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
